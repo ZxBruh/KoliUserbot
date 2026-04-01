743541a6eab603c77351dbdbc9687a93e6839e6a
ZxBruh <166714385+ZxBruh@users.noreply.github.com>
2026-04-01 23:33:50 +0700

    Delete ping.py

--- modules/ping.py
+++
@@ -1,9 +0,0 @@
-from telethon import events
-import time
-
-@events.register(events.NewMessage(outgoing=True, pattern=r"\.пинг"))
-async def ping(event):
-    start = time.time()
-    await event.edit("<code>Проверка...</code>")
-    end = time.time()
-    await event.edit(f"<b>🏓 Понг!</b>\n<code>{(end - start) * 1000:.2f}мс</code>")
